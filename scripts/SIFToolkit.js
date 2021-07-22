08a0a8c2647662c7efe649c110dc8ea9f6b1e53f
Nathan <nldegarmo@gmail.com>
2021-07-22 13:12:48 -0400

    updated version number.

--- scripts/SIFToolkit.js
+++ scripts/SIFToolkit.js
@@ -12,7 +12,7 @@ class  siftoolkit{
 	static textures = undefined;
 	static SIFData = {};
 	static soundHold = true;
-	static version = "0.1.12";
+	static version = "0.1.13";
 	static openItems = {};
 }
 globalThis.SIFT = siftoolkit;
