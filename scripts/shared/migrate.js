d094c070e55adcc4112fdedacf703ef2e57e1a80
bitkiller0 <70086485+bitkiller0@users.noreply.github.com>
2021-07-27 08:14:04 -0400

    Update migrate.js

--- scripts/shared/migrate.js
+++ scripts/shared/migrate.js
@@ -56,7 +56,7 @@ export function migrate(){
             });
 
         default:
-            game.settings.set("siftoolkit","SIFTVersion","0.1.17");            
+            game.settings.set("siftoolkit","SIFTVersion","0.1.18");            
             ui.notifications.info("SIFToolkit upgrade complete.");
     }
 }
