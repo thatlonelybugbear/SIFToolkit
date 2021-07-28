cecc8009d6a41de982099ac8f3b0cf500e979a47
bitkiller0 <70086485+bitkiller0@users.noreply.github.com>
2021-07-28 18:01:32 -0400

    Add files via upload

---
+++ SIFToolkit.js
@@ -0,0 +1,30 @@
+import {prepareSIFT} from './prepareSIFT.js';
+import {loadUtils} from './loadUtils.js';
+import {loadUI} from './loadUI.js';
+import {loadTextures} from './loadTextures.js';
+
+class  siftoolkit{
+	static SIFTTT = undefined;
+	static Status = { running : false };
+	static Settings = {};
+	static utils = undefined;
+	static UI = undefined;
+	static textures = undefined;
+	static SIFData = {};
+	static soundHold = true;
+	static version = "0.1.21";
+	static openItems = {};
+}
+globalThis.SIFT = siftoolkit;
+
+prepareSIFT();
+loadUtils();
+loadUI();
+loadTextures();
+
+
+siftoolkit.Status.running = true;
+setTimeout(()=>{SIFT.soundHold = false;},5000);
+
+
+
