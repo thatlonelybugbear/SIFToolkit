b6fd5d0c119f5c63eae681588f119478544ccccd
bitkiller0 <70086485+bitkiller0@users.noreply.github.com>
2021-07-28 20:49:53 -0400

    Update SIFToolkit.js

--- scripts/SIFToolkit.js
+++ scripts/SIFToolkit.js
@@ -12,7 +12,7 @@ class  siftoolkit{
 	static textures = undefined;
 	static SIFData = {};
 	static soundHold = true;
-	static version = "0.1.22";
+	static version = "0.1.23";
 	static openItems = {};
 }
 globalThis.SIFT = siftoolkit;
